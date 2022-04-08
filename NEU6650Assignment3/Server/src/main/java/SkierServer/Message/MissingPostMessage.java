796e3061c8274f203d9e1f4a203c9ced474288b8
lucyhoney <lucy.xia@yahoo.com>
2022-04-07 22:41:52 -0700

    Assignment3

---
+++ NEU6650Assignment3/Server/src/main/java/SkierServer/Message/MissingPostMessage.java
@@ -0,0 +1,9 @@
+package SkierServer.Message;
+
+public class MissingPostMessage {
+    private final String message;
+
+    public MissingPostMessage() {
+        message = "Missing POST parameters!";
+    }
+}
